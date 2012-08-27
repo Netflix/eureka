002410dceff1d066a08e0f1819540dc2cb74f683
Karthik Ranganathan <kranganathan@netflix.com>
2012-08-26 23:09:08 -0700

    Configuration cleanup.

--- eureka-client/src/main/java/com/netflix/discovery/provider/DiscoveryJerseyProvider.java
+++ eureka-client/src/main/java/com/netflix/discovery/provider/DiscoveryJerseyProvider.java
@@ -19,6 +19,7 @@ package com.netflix.discovery.provider;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Serializable;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.concurrent.ConcurrentHashMap;
