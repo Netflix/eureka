21c0d97be504695f70979e3d0f746cf6b185be04
Karthik Ranganathan <kranganathan@netflix.com>
2012-12-23 17:11:06 -0800

    https://github.com/Netflix/blitz4j/issues/1:
    1) Added missing test dependencies
    2) Added some missing license headers.

--- src/test/java/com/netflix/blitz4j/TestBlitz4j.java
+++ src/test/java/com/netflix/blitz4j/TestBlitz4j.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Netflix, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.netflix.blitz4j;
 
 import java.util.Collection;
@@ -156,6 +172,7 @@ public class TestBlitz4j {
 
         Thread.sleep(4000);
         Assert.assertFalse(slfLogger.isDebugEnabled());
+        LoggingConfiguration.getInstance().stop();
     }
 
     private String getMonitoringData(String metricName) {
