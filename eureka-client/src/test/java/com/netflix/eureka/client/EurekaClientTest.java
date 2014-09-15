3b090625ecaa5aa8002a2f394f83491f83f8bd2d
David Liu <dliu@netflix.com>
2014-09-15 14:22:23 -0700

    Some more unit tests

--- eureka-client/src/test/java/com/netflix/eureka/client/EurekaClientTest.java
+++ eureka-client/src/test/java/com/netflix/eureka/client/EurekaClientTest.java
@@ -4,6 +4,7 @@ import com.netflix.eureka.client.service.EurekaClientService;
 import com.netflix.eureka.interests.ChangeNotification;
 import com.netflix.eureka.interests.Interest;
 import com.netflix.eureka.interests.Interests;
+import com.netflix.eureka.interests.MultipleInterests;
 import com.netflix.eureka.interests.SampleChangeNotification;
 import com.netflix.eureka.registry.EurekaRegistry;
 import com.netflix.eureka.registry.InstanceInfo;
@@ -15,10 +16,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExternalResource;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.functions.Func1;
 
 import java.util.ArrayList;
@@ -31,6 +36,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
 /**
@@ -82,6 +88,7 @@ public class EurekaClientTest {
                 registry.register(notification.getData());
             }
 
+            // interest channel mocks
             when(interestChannel.register(eq(interestAll))).thenReturn(mockInterestStream);
             when(interestChannel.register(eq(interestDiscovery))).thenReturn(mockInterestStream.filter(new Func1<ChangeNotification<InstanceInfo>, Boolean>() {
                 @Override
@@ -99,10 +106,23 @@ public class EurekaClientTest {
             when(interestChannel.upgrade(eq(interestDiscovery))).thenReturn(Observable.<Void>empty());
             when(interestChannel.upgrade(eq(interestZuul))).thenReturn(Observable.<Void>empty());
 
+            // eureka service mocks that does pass through to the registry
             when(eurekaService.newInterestChannel()).thenReturn(interestChannel);
-            when(eurekaService.forInterest(eq(interestAll))).thenReturn(registry.forInterest(interestAll));
-            when(eurekaService.forInterest(eq(interestDiscovery))).thenReturn(registry.forInterest(interestDiscovery));
-            when(eurekaService.forInterest(eq(interestZuul))).thenReturn(registry.forInterest(interestZuul));
+            when(eurekaService.forInterest(Matchers.any(Interest.class))).thenAnswer(new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    Object[] args = invocation.getArguments();
+                    Object mock = invocation.getMock();
+                    return registry.forInterest((Interest<InstanceInfo>) args[0]);
+                }
+            });
+            doAnswer(new Answer() {
+                @Override
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    registry.shutdown();
+                    return null;
+                }
+            }).when(eurekaService).shutdown();
 
             processor = new InterestProcessor(interestChannel);
 
@@ -116,13 +136,6 @@ public class EurekaClientTest {
         }
     };
 
-
-
-
-    @Test
-    public void testCloseClient() {
-
-    }
 
     // =======================
     // registration path tests
@@ -156,7 +169,7 @@ public class EurekaClientTest {
                 output.add(notification);
                 latch.countDown();
                 if (latch.getCount() == 1) {
-                    registry.shutdown();  // this sends an onComplete to the stream
+                    eurekaService.shutdown();  // this sends an onComplete to the stream
                 }
             }
         });
@@ -170,11 +183,13 @@ public class EurekaClientTest {
     public void testForInterestSameTwoUsers() throws Exception {
         final List<ChangeNotification<InstanceInfo>> output1 = new ArrayList<>();
 
+        final CountDownLatch completionLatch = new CountDownLatch(2);
+
         final CountDownLatch latch1 = new CountDownLatch(4);
         client.forInterest(interestAll).subscribe(new Subscriber<ChangeNotification<InstanceInfo>>() {
             @Override
             public void onCompleted() {
-                Assert.fail("the stream doesn't onComplete until registry is shutdown");
+                completionLatch.countDown();
             }
 
             @Override
@@ -195,7 +210,7 @@ public class EurekaClientTest {
         client.forInterest(interestAll).subscribe(new Subscriber<ChangeNotification<InstanceInfo>>() {
             @Override
             public void onCompleted() {
-                Assert.fail("the stream doesn't onComplete until registry is shutdown");
+                completionLatch.countDown();
             }
 
             @Override
@@ -215,17 +230,22 @@ public class EurekaClientTest {
 
         assertThat(output1, containsInAnyOrder(allRegistry.toArray()));
         assertThat(output2, containsInAnyOrder(allRegistry.toArray()));
+
+        client.close();
+        assertThat(completionLatch.await(1, TimeUnit.MINUTES), equalTo(true));
     }
 
     @Test
     public void testForInterestDifferentTwoUsers() throws Exception {
         final List<ChangeNotification<InstanceInfo>> discoveryOutput = new ArrayList<>();
 
+        final CountDownLatch completionLatch = new CountDownLatch(2);
+
         final CountDownLatch latch1 = new CountDownLatch(2);
         client.forInterest(interestDiscovery).subscribe(new Subscriber<ChangeNotification<InstanceInfo>>() {
             @Override
             public void onCompleted() {
-                Assert.fail("the stream doesn't onComplete until registry is shutdown");
+                completionLatch.countDown();
             }
 
             @Override
@@ -246,7 +266,7 @@ public class EurekaClientTest {
         client.forInterest(interestZuul).subscribe(new Subscriber<ChangeNotification<InstanceInfo>>() {
             @Override
             public void onCompleted() {
-                Assert.fail("the stream doesn't onComplete until registry is shutdown");
+                completionLatch.countDown();
             }
 
             @Override
@@ -262,15 +282,78 @@ public class EurekaClientTest {
         });
 
         assertThat(latch1.await(1, TimeUnit.MINUTES), equalTo(true));
-        assertThat(latch1.await(1, TimeUnit.MINUTES), equalTo(true));
+        assertThat(latch2.await(1, TimeUnit.MINUTES), equalTo(true));
 
         assertThat(discoveryOutput, containsInAnyOrder(discoveryRegistry.toArray()));
         assertThat(zuulOutput, containsInAnyOrder(zuulRegistry.toArray()));
+
+        client.close();
+        assertThat(completionLatch.await(1, TimeUnit.MINUTES), equalTo(true));
     }
 
     @Test
-    public void testForInterestSecondInterestSupercedeFirst() {
+    public void testForInterestSecondInterestSupercedeFirst() throws Exception {
+        final List<ChangeNotification<InstanceInfo>> discoveryOutput = new ArrayList<>();
 
+        final CountDownLatch completionLatch = new CountDownLatch(2);
+
+        final CountDownLatch latch1 = new CountDownLatch(2);
+        client.forInterest(interestDiscovery)
+                .subscribe(new Subscriber<ChangeNotification<InstanceInfo>>() {
+                    @Override
+                    public void onCompleted() {
+                        completionLatch.countDown();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Assert.fail("should not onError");
+                    }
+
+                    @Override
+                    public void onNext(ChangeNotification<InstanceInfo> notification) {
+                        discoveryOutput.add(notification);
+                        latch1.countDown();
+                    }
+                });
+
+        // don't use all registry interest as it is a special singleton
+        Interest<InstanceInfo> compositeInterest = new MultipleInterests<>(interestDiscovery, interestZuul);
+        when(interestChannel.upgrade(eq(compositeInterest))).thenReturn(Observable.<Void>empty());
+
+        final List<ChangeNotification<InstanceInfo>> compositeOutput = new ArrayList<>();
+
+        final CountDownLatch latch2 = new CountDownLatch(2);
+        client.forInterest(compositeInterest)
+                .subscribe(new Subscriber<ChangeNotification<InstanceInfo>>() {
+                    @Override
+                    public void onCompleted() {
+                        completionLatch.countDown();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Assert.fail("should not onError");
+                    }
+
+                    @Override
+                    public void onNext(ChangeNotification<InstanceInfo> notification) {
+                        compositeOutput.add(notification);
+                        latch2.countDown();
+                    }
+                });
+
+        assertThat(latch1.await(1, TimeUnit.MINUTES), equalTo(true));
+        assertThat(latch1.await(2, TimeUnit.MINUTES), equalTo(true));
+
+        assertThat(discoveryOutput, containsInAnyOrder(discoveryRegistry.toArray()));
+
+        List<ChangeNotification<InstanceInfo>> compositeRegistry = new ArrayList<>(discoveryRegistry);
+        compositeRegistry.addAll(zuulRegistry);
+        assertThat(compositeOutput, containsInAnyOrder(compositeRegistry.toArray()));
+
+        client.close();
+        assertThat(completionLatch.await(1, TimeUnit.MINUTES), equalTo(true));
     }
 
     @Test
