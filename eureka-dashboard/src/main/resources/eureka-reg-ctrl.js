0250c0dd2b8f682aa473fd950ba3de62a51fb505
Amit Joshi <ajoshi@netflix.com>
2014-11-25 18:33:20 -0800

    live streaming feed view

--- eureka-dashboard/src/main/resources/eureka-reg-ctrl.js
+++ eureka-dashboard/src/main/resources/eureka-reg-ctrl.js
@@ -1,6 +1,4 @@
 var eurekaRegistryCtrl = (function () {
-    var registry = {};
-
     function load() {
         var ws = new WebSocket('ws://localhost:9000');
         ws.onopen = function () {
@@ -8,38 +6,12 @@ var eurekaRegistryCtrl = (function () {
         };
         ws.onmessage = function (data, flags) {
             var instNotification = JSON.parse(data.data);
-            if (instNotification.kind === 'Add') {
-                console.log("Add - " + instNotification.data['id']);
-                addInstance(instNotification.data);
-            } else if (instNotification.kind === 'Remove') {
-                console.log("Remove - " + instNotification.data['id']);
-                removeInstance(instNotification.data);
-            }
+            $(window).trigger('InstanceNotificationReceived', {instInfo : instNotification.data, type : instNotification.kind});
         };
-    }
-    
-    function addInstance(instInfo) {
-        if (instInfo['app'] in registry) {
-            registry[instInfo['app']].push(instInfo);
-        } else {
-            registry[instInfo['app']] = [instInfo];
-        }
-    }
-    
-    function removeInstance(instInfo) {
-        if (instInfo['app'] in registry) {
-            registry[instInfo['app']] = registry[instInfo['app']].filter(function (elm) {
-                return elm['id'] !== instInfo['id'];
-            });
-        }
-    }
-
-    function getRegistry() {
-        return registry;
     }
 
     return {
-        load : load,
-        getRegistry : getRegistry
+        load : load
     }
+
 })();
