75bd72d67d1452986dc02c092ee6edc1da55b690
Tomasz Bak <tbak@netflix.com>
2014-12-31 09:33:35 -0800

    Update sample data classes.

---
+++ eureka-testkit/src/test/java/com/netflix/eureka2/testkit/data/builder/SampleAwsDataCenterInfoTest.java
@@ -0,0 +1,30 @@
+package com.netflix.eureka2.testkit.data.builder;
+
+import java.util.Iterator;
+
+import com.netflix.eureka2.registry.datacenter.AwsDataCenterInfo;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Tomasz Bak
+ */
+public class SampleAwsDataCenterInfoTest {
+
+    @Test
+    public void testGeneratesSubsequentDataCenterInfos() throws Exception {
+        Iterator<AwsDataCenterInfo> iterator =
+                SampleAwsDataCenterInfo.collectionOf("test", SampleAwsDataCenterInfo.UsEast1a.build());
+
+        AwsDataCenterInfo first = iterator.next();
+        assertThat(first.getPublicAddress().getHostName(), containsString("test"));
+        assertThat(first.getInstanceId(), is(equalTo("id-00000001")));
+
+        AwsDataCenterInfo second = iterator.next();
+        assertThat(second.getInstanceId(), is(equalTo("id-00000002")));
+    }
+}
---
+++ eureka-testkit/src/test/java/com/netflix/eureka2/testkit/data/builder/SampleNetworkAddressTest.java
@@ -0,0 +1,39 @@
+package com.netflix.eureka2.testkit.data.builder;
+
+import java.util.Iterator;
+
+import com.netflix.eureka2.registry.NetworkAddress;
+import com.netflix.eureka2.registry.NetworkAddress.ProtocolType;
+import org.junit.Test;
+
+import static com.netflix.eureka2.registry.NetworkAddress.NetworkAddressBuilder.aNetworkAddress;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Tomasz Bak
+ */
+public class SampleNetworkAddressTest {
+
+    @Test
+    public void testGeneratesSubsequentIpAddresses() throws Exception {
+        Iterator<NetworkAddress> addressIterator = SampleNetworkAddress.collectionOfIPv4("20", "test.internal", "private");
+
+        NetworkAddress expectedFirst = aNetworkAddress()
+                .withHostName("ip20_0_0_1.test.internal")
+                .withIpAddress("20.0.0.1")
+                .withLabel("private")
+                .withProtocolType(ProtocolType.IPv4)
+                .build();
+        assertThat(addressIterator.next(), is(equalTo(expectedFirst)));
+
+        NetworkAddress expectedSecond = aNetworkAddress()
+                .withHostName("ip20_0_0_2.test.internal")
+                .withIpAddress("20.0.0.2")
+                .withLabel("private")
+                .withProtocolType(ProtocolType.IPv4)
+                .build();
+        assertThat(addressIterator.next(), is(equalTo(expectedSecond)));
+    }
+}
