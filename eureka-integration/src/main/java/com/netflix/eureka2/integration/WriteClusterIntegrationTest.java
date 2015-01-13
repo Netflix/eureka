6fafa4621cfa9cfaf88ec7febb279f02d44ca798
Tomasz Bak <tbak@netflix.com>
2015-01-13 12:55:48 -0800

    Fix flaky integration test - second update.

--- eureka-integration/src/main/java/com/netflix/eureka2/integration/WriteClusterIntegrationTest.java
+++ eureka-integration/src/main/java/com/netflix/eureka2/integration/WriteClusterIntegrationTest.java
@@ -3,23 +3,28 @@ package com.netflix.eureka2.integration;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 import com.netflix.eureka2.client.EurekaClient;
-import com.netflix.eureka2.junit.categories.IntegrationTest;
 import com.netflix.eureka2.interests.ChangeNotification;
 import com.netflix.eureka2.interests.Interests;
+import com.netflix.eureka2.junit.categories.IntegrationTest;
 import com.netflix.eureka2.registry.instance.InstanceInfo;
 import com.netflix.eureka2.testkit.data.builder.SampleInstanceInfo;
 import com.netflix.eureka2.testkit.junit.resources.EurekaDeploymentResource;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import rx.functions.Action1;
 
-import static com.netflix.eureka2.rx.RxBlocking.*;
-import static com.netflix.eureka2.testkit.junit.EurekaMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import static com.netflix.eureka2.rx.RxBlocking.iteratorFrom;
+import static com.netflix.eureka2.testkit.junit.EurekaMatchers.addChangeNotificationOf;
+import static com.netflix.eureka2.testkit.junit.EurekaMatchers.deleteChangeNotificationOf;
+import static com.netflix.eureka2.testkit.junit.EurekaMatchers.modifyChangeNotificationOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * FIXME fix replication
@@ -77,20 +82,25 @@ public class WriteClusterIntegrationTest {
         );
 
         // Subscribe to second write server
-        Iterator<ChangeNotification<InstanceInfo>> notificationIterator =
-                iteratorFrom(10, TimeUnit.SECONDS, discoveryClient.forApplication(infos.get(0).getApp()));
+        final BlockingQueue<ChangeNotification<InstanceInfo>> notifications = new LinkedBlockingQueue<>();
+        discoveryClient.forApplication(infos.get(0).getApp()).subscribe(new Action1<ChangeNotification<InstanceInfo>>() {
+            @Override
+            public void call(ChangeNotification<InstanceInfo> notification) {
+                notifications.add(notification);
+            }
+        });
 
         // We need to block, otherwise if we shot all of them in one row, they will be processed
         // in order, but subscribe operations may eventually re-ordered them.
         registrationClient.register(infos.get(0)).toBlocking().firstOrDefault(null);
         registrationClient.register(infos.get(1)).toBlocking().firstOrDefault(null);
         registrationClient.register(infos.get(2)).toBlocking().firstOrDefault(null);
-        registrationClient.unregister(infos.get(2)).subscribe();
+        registrationClient.unregister(infos.get(2)).toBlocking().firstOrDefault(null);
 
-        assertThat(notificationIterator.next(), is(addChangeNotificationOf(infos.get(0))));
-        assertThat(notificationIterator.next(), is(modifyChangeNotificationOf(infos.get(1))));
-        assertThat(notificationIterator.next(), is(modifyChangeNotificationOf(infos.get(2))));
-        assertThat(notificationIterator.next(), is(deleteChangeNotificationOf(infos.get(2))));
+        assertThat(notifications.poll(10, TimeUnit.SECONDS), is(addChangeNotificationOf(infos.get(0))));
+        assertThat(notifications.poll(10, TimeUnit.SECONDS), is(modifyChangeNotificationOf(infos.get(1))));
+        assertThat(notifications.poll(10, TimeUnit.SECONDS), is(modifyChangeNotificationOf(infos.get(2))));
+        assertThat(notifications.poll(10, TimeUnit.SECONDS), is(deleteChangeNotificationOf(infos.get(2))));
 
         registrationClient.close();
         discoveryClient.close();
