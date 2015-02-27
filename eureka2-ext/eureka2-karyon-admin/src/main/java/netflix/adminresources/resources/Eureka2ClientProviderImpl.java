d841fdc7d369af34e8449f2baf6e49a1adf3c382
Tomasz Bak <tbak@netflix.com>
2015-02-27 13:49:04 -0800

    Configuration update to work properly in local environment.

--- eureka2-ext/eureka2-karyon-admin/src/main/java/netflix/adminresources/resources/Eureka2ClientProviderImpl.java
+++ eureka2-ext/eureka2-karyon-admin/src/main/java/netflix/adminresources/resources/Eureka2ClientProviderImpl.java
@@ -5,9 +5,14 @@ import com.netflix.config.ConfigurationManager;
 import com.netflix.eureka2.client.EurekaClientBuilder;
 import com.netflix.eureka2.client.interest.EurekaInterestClient;
 import com.netflix.eureka2.client.resolver.ServerResolvers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class Eureka2ClientProviderImpl implements Eureka2ClientProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(Eureka2ClientProviderImpl.class);
+
     public static final String CONFIG_DISCOVERY_PORT = "eureka.client.discovery-endpoint.port";
     public static final String CONFIG_DISCOVERY_DNS = "eureka.client.discovery-endpoint.dns";
     private int port = ConfigurationManager.getConfigInstance().getInt(CONFIG_DISCOVERY_PORT, 12001);
@@ -15,6 +20,7 @@ public class Eureka2ClientProviderImpl implements Eureka2ClientProvider {
 
     @Override
     public EurekaInterestClient get() {
+        logger.info("Subscribing to Eureka2 server {}:{}", discoveryDNS, port);
         return EurekaClientBuilder
                 .discoveryBuilder()
                 .withReadServerResolver(ServerResolvers.forDnsName(discoveryDNS, port))
