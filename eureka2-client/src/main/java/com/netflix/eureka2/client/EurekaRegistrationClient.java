97857273816a2b9b9c83ca93f38bb1a1fd64da95
David Liu <dliu@netflix.com>
2015-02-27 17:02:28 -0800

    Comments fixes

--- eureka2-client/src/main/java/com/netflix/eureka2/client/EurekaRegistrationClient.java
+++ eureka2-client/src/main/java/com/netflix/eureka2/client/EurekaRegistrationClient.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.netflix.eureka2.client;
 
 import com.netflix.eureka2.client.registration.RegistrationObservable;
@@ -16,7 +32,7 @@ import rx.Observable;
 public interface EurekaRegistrationClient {
 
     /**
-     * Return a {@link com.netflix.eureka2.client.registration.RegistrationObservable} that when subscribes to, initiates registration with the remote server
+     * Return a {@link RegistrationObservable} that when subscribes to, initiates registration with the remote server
      * based on the InstanceInfos received. Changes between InstanceInfos will be applied as updates to the initial
      * registration. InstanceInfo Ids cannot change for InstanceInfos within an input stream.
      *
@@ -24,7 +40,7 @@ public interface EurekaRegistrationClient {
      *                   updated. Initial registrations is predicated on two conditions, the returned
      *                   RegistrationRequest must be subscribed to, and an initial InstanceInfo must be emitted
      *                   by the input observable.
-     * @return {@link com.netflix.eureka2.client.registration.RegistrationObservable}
+     * @return {@link RegistrationObservable}
      */
     RegistrationObservable register(Observable<InstanceInfo> registrant);
 
