0a539722f42cc5ccefb794c0619b2057678cf6b8
Spencer Gibb <spencer@gibb.us>
2015-05-19 13:34:36 -0600

    make constructor public so other DI systems can create instances

--- eureka-client/src/main/java/com/netflix/appinfo/ApplicationInfoManager.java
+++ eureka-client/src/main/java/com/netflix/appinfo/ApplicationInfoManager.java
@@ -56,7 +56,7 @@ public class ApplicationInfoManager {
     }
 
     @Inject
-    ApplicationInfoManager(EurekaInstanceConfig config, InstanceInfo instanceInfo) {
+    public ApplicationInfoManager(EurekaInstanceConfig config, InstanceInfo instanceInfo) {
         this.config = config;
         this.instanceInfo = instanceInfo;
 
