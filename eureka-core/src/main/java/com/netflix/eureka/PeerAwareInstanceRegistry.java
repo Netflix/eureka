df713041dd8f14d56669764ff4008377ff3a1a21
David Liu <dliu@netflix.com>
2015-06-05 13:19:40 -0700

    Fix inconsistency with selfPreservation check and how it's reported as servo metrics

--- eureka-core/src/main/java/com/netflix/eureka/PeerAwareInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/PeerAwareInstanceRegistry.java
@@ -674,14 +674,14 @@ public class PeerAwareInstanceRegistry extends InstanceRegistry {
     }
 
     /**
-     * Checks if the number of renewals is lesser than threshold.
+     * Checks if the number of renewals is lesser than threshold. This method is for servo metrics only.
      *
      * @return 0 if the renewals are greater than threshold, 1 otherwise.
      */
     @com.netflix.servo.annotations.Monitor(name = "isBelowRenewThreshold", description = "0 = false, 1 = true",
             type = com.netflix.servo.annotations.DataSourceType.GAUGE)
     public int isBelowRenewThresold() {
-        if ((getNumOfRenewsInLastMin() < numberOfRenewsPerMinThreshold)
+        if ((getNumOfRenewsInLastMin() <= numberOfRenewsPerMinThreshold)
                 && ((this.startupTime > 0) && (System.currentTimeMillis() > this.startupTime
                 + (EUREKA_SERVER_CONFIG.getWaitTimeInMsWhenSyncEmpty())))) {
             return 1;
