292e3885e46f5b9f7ce16599ec11bed6d853f7e1
Tomasz Bak <tbak@netflix.com>
2015-10-04 22:51:50 -0700

    More test refactoring

--- eureka-client/src/test/java/com/netflix/discovery/DiscoveryClientEventBusTest.java
+++ eureka-client/src/test/java/com/netflix/discovery/DiscoveryClientEventBusTest.java
@@ -1,100 +1,84 @@
 package com.netflix.discovery;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Injector;
-import com.google.inject.Module;
-import com.google.inject.Scopes;
-import com.google.inject.util.Modules;
-import com.netflix.appinfo.ApplicationInfoManager;
-import com.netflix.appinfo.EurekaInstanceConfig;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.appinfo.providers.MyDataCenterInstanceConfigProvider;
-import com.netflix.config.ConfigurationManager;
-import com.netflix.discovery.guice.EurekaModule;
-import com.netflix.eventbus.impl.EventBusImpl;
-import com.netflix.eventbus.spi.EventBus;
-import com.netflix.eventbus.spi.Subscribe;
-import com.netflix.governator.guice.LifecycleInjector;
-import com.netflix.governator.guice.LifecycleInjectorBuilder;
-import com.netflix.governator.guice.LifecycleInjectorMode;
-import com.netflix.governator.lifecycle.LifecycleManager;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.junit.resource.DiscoveryClientResource;
+import com.netflix.discovery.shared.Applications;
+import com.netflix.discovery.shared.transport.EurekaHttpClient;
+import com.netflix.discovery.shared.transport.EurekaHttpResponse;
+import com.netflix.discovery.shared.transport.SimpleEurekaHttpServer;
+import com.netflix.discovery.util.InstanceInfoGenerator;
+import com.netflix.eventbus.spi.EventBus;
+import com.netflix.eventbus.spi.Subscribe;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.netflix.discovery.shared.transport.EurekaHttpResponse.anEurekaHttpResponse;
+import static com.netflix.discovery.util.EurekaEntityFunctions.toApplications;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 /**
  * @author David Liu
  */
-public class DiscoveryClientEventBusTest extends BaseDiscoveryClientTester {
+public class DiscoveryClientEventBusTest {
 
-    private Injector injector;
-    private LifecycleManager lifecycleManager;
+    private static final EurekaHttpClient requestHandler = mock(EurekaHttpClient.class);
+    private static SimpleEurekaHttpServer eurekaHttpServer;
+
+    @Rule
+    public DiscoveryClientResource discoveryClientResource = DiscoveryClientResource.newBuilder()
+            .withRegistration(true)
+            .withRegistryFetch(true)
+            .connectWith(eurekaHttpServer)
+            .build();
+
+    /**
+     * Share server stub by all tests.
+     */
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        eurekaHttpServer = new SimpleEurekaHttpServer(requestHandler);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        if (eurekaHttpServer != null) {
+            eurekaHttpServer.shutdown();
+        }
+    }
 
     @Before
     public void setUp() throws Exception {
-
-        setupProperties();
-        // override to make the test faster
-        ConfigurationManager.getConfigInstance().setProperty("eureka.client.refresh.interval", 2);
-
-        // disable delta as the mock server does not handle register and subsequent deltas
-        ConfigurationManager.getConfigInstance().setProperty("eureka.disableDelta", true);
-
-        // override to test registration path
-        ConfigurationManager.getConfigInstance().setProperty("eureka.name", "EurekaTestApp-" + UUID.randomUUID());
-        ConfigurationManager.getConfigInstance().setProperty("eureka.registration.enabled", "true");
-        ConfigurationManager.getConfigInstance().setProperty("eureka.appinfo.initial.replicate.time", "0");
-        ConfigurationManager.getConfigInstance().setProperty("eureka.appinfo.replicate.interval", "5");
-        ConfigurationManager.getConfigInstance().setProperty("eureka.serviceUrl.default",
-                "http://localhost:" + mockLocalEurekaServer.getPort() + MockRemoteEurekaServer.EUREKA_API_BASE_PATH);
-
-        populateLocalRegistryAtStartup();
-        populateRemoteRegistryAtStartup();
-
-        // Simulated Child Injectors required for the optional @Injects in DiscoveryClient
-        LifecycleInjectorBuilder builder = LifecycleInjector.builder().withMode(LifecycleInjectorMode.SIMULATED_CHILD_INJECTORS);
-
-        Module testModule = Modules.override(new EurekaModule()).with(new AbstractModule() {
-            @Override
-            protected void configure() {
-                // the default impl of EurekaInstanceConfig is CloudInstanceConfig, which we only want in an AWS
-                // environment. Here we override that by binding MyDataCenterInstanceConfig to EurekaInstanceConfig.
-                bind(EurekaInstanceConfig.class).toProvider(MyDataCenterInstanceConfigProvider.class).in(Scopes.SINGLETON);
-
-                // bind the optional event bus
-                bind(EventBus.class).to(EventBusImpl.class).in(Scopes.SINGLETON);
-            }
-        });
-
-        builder.withModules(testModule);
-
-        injector = builder.build().createInjector();
-        lifecycleManager = injector.getInstance(LifecycleManager.class);
-        lifecycleManager.start();
-    }
-
-    @After
-    public void tearDown() {
-        if (lifecycleManager != null) {
-            lifecycleManager.close();
-        }
-
-        ConfigurationManager.getConfigInstance().clear();
+        reset(requestHandler);
+        when(requestHandler.register(any(InstanceInfo.class))).thenReturn(EurekaHttpResponse.status(204));
+        when(requestHandler.cancel(anyString(), anyString())).thenReturn(EurekaHttpResponse.status(200));
+        when(requestHandler.getDelta()).thenReturn(
+                anEurekaHttpResponse(200, new Applications()).type(MediaType.APPLICATION_JSON_TYPE).build()
+        );
     }
 
     @Test
     public void testStatusChangeEvent() throws Exception {
         final CountDownLatch eventLatch = new CountDownLatch(1);
         final List<StatusChangeEvent> receivedEvents = new ArrayList<StatusChangeEvent>();
-
-        EventBus eventBus = injector.getInstance(EventBus.class);
+        EventBus eventBus = discoveryClientResource.getEventBus();
         eventBus.registerSubscriber(new Object() {
             @Subscribe
             public void consume(StatusChangeEvent event) {
@@ -102,30 +86,35 @@ public class DiscoveryClientEventBusTest extends BaseDiscoveryClientTester {
                 eventLatch.countDown();
             }
         });
-        // mark status as down. Should see change on next cache refresh
-        injector.getInstance(ApplicationInfoManager.class).setInstanceStatus(InstanceInfo.InstanceStatus.DOWN);
 
-        Assert.assertTrue(eventLatch.await(10, TimeUnit.SECONDS));
-        Assert.assertEquals(1, receivedEvents.size());
-        Assert.assertNotNull(receivedEvents.get(0));
+        Applications initialApps = toApplications(discoveryClientResource.getMyInstanceInfo());
+        when(requestHandler.getApplications()).thenReturn(
+                anEurekaHttpResponse(200, initialApps).type(MediaType.APPLICATION_JSON_TYPE).build()
+        );
+        discoveryClientResource.getClient(); // Activates the client
+
+        assertThat(eventLatch.await(10, TimeUnit.SECONDS), is(true));
+        assertThat(receivedEvents.size(), is(equalTo(1)));
+        assertThat(receivedEvents.get(0), is(notNullValue()));
     }
 
     @Test
     public void testCacheRefreshEvent() throws Exception {
-        final CountDownLatch eventLatch = new CountDownLatch(1);
-        final List<CacheRefreshedEvent> receivedEvents = new ArrayList<CacheRefreshedEvent>();
+        InstanceInfoGenerator instanceGen = InstanceInfoGenerator.newBuilder(2, "testApp").build();
 
-        EventBus eventBus = injector.getInstance(EventBus.class);
-        eventBus.registerSubscriber(new Object() {
-            @Subscribe
-            public void consume(CacheRefreshedEvent event) {
-                receivedEvents.add(event);
-                eventLatch.countDown();
-            }
-        });
+        // Initial full fetch
+        Applications initialApps = instanceGen.takeDelta(1);
+        when(requestHandler.getApplications()).thenReturn(
+                anEurekaHttpResponse(200, initialApps).type(MediaType.APPLICATION_JSON_TYPE).build()
+        );
+        discoveryClientResource.getClient(); // Activates the client
 
-        Assert.assertTrue(eventLatch.await(5, TimeUnit.SECONDS));
-        Assert.assertEquals(1, receivedEvents.size());
-        Assert.assertNotNull(receivedEvents.get(0));
+        // Delta update
+        Applications delta = instanceGen.takeDelta(1);
+        when(requestHandler.getDelta()).thenReturn(
+                anEurekaHttpResponse(200, delta).type(MediaType.APPLICATION_JSON_TYPE).build()
+        );
+
+        assertThat(discoveryClientResource.awaitCacheUpdate(10, TimeUnit.SECONDS), is(true));
     }
 }
