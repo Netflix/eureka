19844d46dfec5d10630edc42c7a01db5855c12ab
Bill Zhang <jingege.me@gmail.com>
2017-08-08 16:56:33 +0800

    Two phase shutdown

--- eureka-client/src/main/java/com/netflix/discovery/InstanceInfoReplicator.java
+++ eureka-client/src/main/java/com/netflix/discovery/InstanceInfoReplicator.java
@@ -6,6 +6,7 @@ import com.netflix.discovery.util.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
