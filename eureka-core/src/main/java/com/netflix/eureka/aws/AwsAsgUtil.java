e4dd6a7dad8300b965fd34fdf4d578a970442d53
Taras Roshko <troshko@netflix.com>
2018-08-06 12:53:30 -0700

    Address review feedback
    - Disable querying AWS ASGs when using the public API to lookup
    an expired/never cached instance

--- eureka-core/src/main/java/com/netflix/eureka/aws/AwsAsgUtil.java
+++ eureka-core/src/main/java/com/netflix/eureka/aws/AwsAsgUtil.java
@@ -165,6 +165,18 @@ public class AwsAsgUtil implements AsgClient {
         if (result != null) {
             return result;
         } else {
+            if (!serverConfig.shouldUseAwsAsgApi()) {
+                // Disabled, cached values (if any) are still being returned if the caller makes
+                // a decision to call the disabled client during some sort of transitioning
+                // period, but no new values will be fetched while disabled.
+
+                logger.info(("'{}' is not cached at the moment and won't be fetched because querying AWS ASGs "
+                        + "has been disabled via the config, returning the fallback value."),
+                            cacheKey);
+
+                return true;
+            }
+
             logger.info("Cache value for asg {} does not exist yet, async refreshing.", cacheKey.asgName);
             // Only do an async refresh if it does not yet exist. Do this to refrain from calling aws api too much
             asgCache.refresh(cacheKey);
