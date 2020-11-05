5d2db6d1b535ff94be539dc4ca9b8f3253648ad3
Ling Yuan <lyuan@netflix.com>
2020-11-05 09:22:20 -0800

    Make instance config factory support custom amazon info provider

---
+++ eureka-client-archaius2/src/main/java/com/netflix/appinfo/providers/CustomAmazonInfoProviderInstanceConfigFactory.java
@@ -0,0 +1,33 @@
+package com.netflix.appinfo.providers;
+
+import com.netflix.appinfo.AmazonInfo;
+import com.netflix.appinfo.Ec2EurekaArchaius2InstanceConfig;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import com.netflix.archaius.api.Config;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+@Singleton
+public class CustomAmazonInfoProviderInstanceConfigFactory implements EurekaInstanceConfigFactory {
+
+    private final Config configInstance;
+    private final Provider<AmazonInfo> amazonInfoProvider;
+    private EurekaInstanceConfig eurekaInstanceConfig;
+
+    @Inject
+    public CustomAmazonInfoProviderInstanceConfigFactory(Config configInstance, AmazonInfoProviderFactory amazonInfoProviderFactory) {
+        this.configInstance = configInstance;
+        this.amazonInfoProvider = amazonInfoProviderFactory.get();
+    }
+
+    @Override
+    public EurekaInstanceConfig get() {
+        if (eurekaInstanceConfig == null) {
+            eurekaInstanceConfig = new Ec2EurekaArchaius2InstanceConfig(configInstance, amazonInfoProvider);
+        }
+
+        return eurekaInstanceConfig;
+    }
+}
