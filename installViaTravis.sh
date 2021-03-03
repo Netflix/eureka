c43d873b9bc494bd0ff0d6a13d20cfde60b1b3e4
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2021-03-03 15:51:10 -0800

    Upgrade nebula.netflixoss to replace bintray publication and update TravisCI secrets (#1380)

--- installViaTravis.sh
+++ installViaTravis.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 # This script will build the project.
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Assemble Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew assemble
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo -e 'Assemble Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true assemble 
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Assemble Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true assemble
-else
-  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew assemble
+if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
+  echo "Decrypting publishing credentials"
+  openssl aes-256-cbc -k "$NETFLIX_OSS_SIGNING_FILE_PASSWORD" -in secrets/signing-key.enc -out secrets/signing-key -d
 fi
