c316bcd547e3a851423578888242eaf1fafbac81
Brian Tucker <briant@netflix.com>
2023-11-08 09:15:35 -0800

    explicit imports

--- eureka-client/src/test/java/com/netflix/appinfo/AmazonInfoTest.java
+++ eureka-client/src/test/java/com/netflix/appinfo/AmazonInfoTest.java
@@ -11,9 +11,13 @@ import org.mockito.MockedStatic;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mockStatic;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+
 
 /**
  * @author David Liu
