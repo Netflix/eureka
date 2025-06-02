aaf255a19d3970889d34dd6b10e0da96d7d86cb9
Hahn <christopher.hahn@thomsonreuters.com>
2025-06-02 12:20:30 -0500

    Move sdkV2 switch to EurekaBootStrap

--- eureka-core/src/main/java/com/netflix/eureka/aws/AwsBinderDelegateV2.java
+++ eureka-core/src/main/java/com/netflix/eureka/aws/AwsBinderDelegateV2.java
@@ -4,33 +4,31 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.discovery.EurekaClientConfig;
 import com.netflix.eureka.EurekaServerConfig;
 import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
-
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
-public class AwsBinderDelegate implements AwsBinder {
+public class AwsBinderDelegateV2 implements AwsBinder {
 
     private final AwsBinder delegate;
 
     @Inject
-    public AwsBinderDelegate(EurekaServerConfig serverConfig,
-                             EurekaClientConfig clientConfig,
-                             PeerAwareInstanceRegistry registry,
-                             ApplicationInfoManager applicationInfoManager) {
+    public AwsBinderDelegateV2(EurekaServerConfig serverConfig,
+                               EurekaClientConfig clientConfig,
+                               PeerAwareInstanceRegistry registry,
+                               ApplicationInfoManager applicationInfoManager) {
         AwsBindingStrategy bindingStrategy = serverConfig.getBindingStrategy();
-        boolean useAwsSdkV2 = serverConfig.isUseAwsSdkV2();
         switch (bindingStrategy) {
             case ROUTE53:
-                delegate =new Route53Binder(serverConfig, clientConfig, applicationInfoManager);
+                delegate = new Route53BinderV2(serverConfig, clientConfig, applicationInfoManager);
                 break;
             case EIP:
-                delegate = new EIPManager(serverConfig, clientConfig, registry, applicationInfoManager);
+                delegate = new EIPManagerV2(serverConfig, clientConfig, registry, applicationInfoManager);
                 break;
             case ENI:
-                delegate = new ElasticNetworkInterfaceBinder(serverConfig, clientConfig, registry, applicationInfoManager);
+                delegate = new ElasticNetworkInterfaceBinderV2(serverConfig, clientConfig, registry, applicationInfoManager);
                 break;
             default:
                 throw new IllegalArgumentException("Unexpected BindingStrategy " + bindingStrategy);
